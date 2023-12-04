e9894b43bd5e27524d32b5c63f3479ed77c580a7
Jin Huang <54080607+JinHuang0101@users.noreply.github.com>
2023-12-04 15:56:44 -0800

    Update env.py

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
-open_ai_api_key = "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"
+open_ai_api_key = ""
 model_engine = "text-davinci-003"
 
 
 # create request header to call SEC API
-headers = {'User-Agent': "jinhuang922@address.com"}
+headers = {'User-Agent': ""}
